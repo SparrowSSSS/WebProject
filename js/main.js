b14a17584138137f348914d5c7e93a2ba9bb4eef
SparrowSSSS <vlad22832006@gmail.com>
2022-12-17 23:36:15 +0300

    v2.0

--- js/main.js
+++ js/main.js
@@ -38,4 +38,12 @@ $(document).ready(function () {
             $('.filter > div').show(300);
         };
     });
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style'
+      });
 });
