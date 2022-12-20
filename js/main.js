aee4830eb004fab3b1ebaa42004a60979fc91025
SparrowSSSS <vlad22832006@gmail.com>
2022-12-20 20:56:07 +0300

    v3.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    /*Portfolio*/
     $('.portfolio__button[filter="wd"]').click(function () {
         if ($(this).attr('val') == 'off') {
             $('.portfolio__button').attr('val', 'off');
@@ -38,12 +39,39 @@ $(document).ready(function () {
             $('.filter > div').show(300);
         };
     });
+    /*Slider*/
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
         dots: true,
-        dotsClass: 'dots-style'
-      });
+        dotsClass: 'dots-style',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }]
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+    });
+    /*Menu*/
+    $('.menu-icon').click(function () {
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display': 'flex',
+            'flex-direction': 'column'
+        });
+    });
 });
